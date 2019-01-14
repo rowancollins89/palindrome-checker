634846e72dcb82c75a2d38e642bb6bcfa59b8849
Rowan Collins <rowan.collins@netsells.co.uk>
2019-01-14 13:34:38 +0000

    remove string manipulation

--- attempt_2.js
+++ attempt_2.js
@@ -1,21 +1,60 @@
 /*
 To run
 
-node -e 'require("./attempt_1").isPalindrome("INSERT STRING HERE")'
+node -e 'require("./attempt_2").isPalindrome("INSERT STRING HERE")'
 
 */
 
 module.exports.isPalindrome = function (string) {
 	console.log(`You are checking to see if "${string}" is a palindrome without using string manipulation. Aren't you a fancy pants developer!.`);
 
-	// OK maybe only a little string manipulation. 
-	const stringArray = string.toLowerCase().split('');
+	const caseComparison = {
+    	A:'a',
+        B:'b',
+        C:'c',
+        D:'d',
+        E:'e',
+        F:'f',
+        G:'g',
+        H:'h',
+        I:'i',
+        J:'j',
+        K:'k',
+        L:'l',
+        M:'m',
+        N:'n',
+        O:'o',
+        P:'p',
+        Q:'q',
+        R:'r',
+        S:'s',
+        T:'t',
+        U:'u',
+        V:'v',
+        W:'w',
+        X:'x',
+        Y:'y',
+        Z:'z',
+	}
 
-	const filteredArray = stringArray.filter((e) => {
+
+	const stringArray = Array.from(string);
+
+	// remove the spaces and commas
+	let filteredArray = stringArray.filter((e) => {
 		return e !== " " && e !== ",";
 	});
 
+	// convert to lowecase
+	filteredArray = filteredArray.map((e) => {
+		if (typeof caseComparison[e] === 'string') {
+			console.log(typeof caseComparison[e])
+			return e = caseComparison[e];
+		}
+		return e;
+	})
 
+	// check string is the same in reverse
 	if (filteredArray.join('') !== filteredArray.reverse().join('')) {
 		return console.log("Get outta here! Do you even know what a palindrome is?");
 	}
